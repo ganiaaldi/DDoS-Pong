d1c87d8c1741af6550498c29bf4c058113925dbf
ganiaaldi <mrgad98@gmail.com>
2020-03-21 10:16:23 +0700

    Add PaddleController and background assets

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+
+    public float wallUpper;
+    public float wallUnder;
+    public float speed;
+    public string axis;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(axis) * speed * Time.deltaTime;
+        float nextPos = transform.position.y + move;
+        if (nextPos > wallUpper ) {
+          move = 0;
+            }
+        if (nextPos < wallUnder) {
+          move = 0;
+        }
+        transform.Translate (0,move,0);
+        
+    }
+}
