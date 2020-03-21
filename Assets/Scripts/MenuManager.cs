d3e1224616fa714e14f701ddf14ac1ff37a40af4
ganiaaldi <mrgad98@gmail.com>
2020-03-21 14:46:59 +0700

    Add main menu and button script

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+     public bool isEscapeToExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetKeyUp (KeyCode.Escape)) {
+            if (isEscapeToExit) {
+                Application.Quit ();
+            } 
+            else {
+                BackToMenu ();
+            }
+        }
+    }
+  
+    public void StartGame () {
+        SceneManager.LoadScene ("SampleScene");
+    }
+  
+    public void BackToMenu () {
+        SceneManager.LoadScene ("Menu");
+    }
+    
+}
