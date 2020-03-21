fd3549ddf051352b62b6c7d5427a5096881f36ac
ganiaaldi <mrgad98@gmail.com>
2020-03-21 13:42:43 +0700

    Add scoring

--- Assets/Scripts/VirusController.cs
+++ Assets/Scripts/VirusController.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VirusController : MonoBehaviour
 {
    public int force;
     Rigidbody2D rigid;
+    int scoreP1;
+    int scoreP2;
+    Text scoreUIP1;
+    Text scoreUIP2;
+    GameObject panelFinish;
+    Text txWinner;
  
     // Use this for initialization    
     void Start () {
         rigid = GetComponent<Rigidbody2D>();
         Vector2 direct = new Vector2(2, 0).normalized;
         rigid.AddForce(direct * force);    
+         scoreP1 = 0;
+        scoreP2 = 0;
+        scoreUIP1 = GameObject.Find ("Score1").GetComponent<Text> ();
+        scoreUIP2 = GameObject.Find ("Score2").GetComponent<Text> ();
+         panelFinish = GameObject.Find("Finish");
+        panelFinish.SetActive(false);
     }
   
     // Update is called once per frame
@@ -21,11 +34,29 @@ public class VirusController : MonoBehaviour
  
     private void OnCollisionEnter2D(Collision2D coll) {
         if (coll.gameObject.name == "Rightside") {
+            scoreP1 += 1;
+            TampilkanScore ();
+            if (scoreP1 == 5) {
+                panelFinish.SetActive(true);
+                txWinner = GameObject.Find("Winner").GetComponent<Text>();
+                txWinner.text = "Whitehat Winner!";
+                Destroy(gameObject);
+                return;
+            }
             ResetBall();
             Vector2 direct = new Vector2(2, 0).normalized;
             rigid.AddForce(direct * force);
         }
         if (coll.gameObject.name == "Leftside") {
+             scoreP2 += 1;
+             TampilkanScore ();
+             if (scoreP2 == 5) {
+                panelFinish.SetActive(true);
+                txWinner = GameObject.Find("Winner").GetComponent<Text>();
+                txWinner.text = "Blackhat Winner!";
+                Destroy(gameObject);
+                return;
+            }
             ResetBall();
             Vector2 direct = new Vector2(-2, 0).normalized;
             rigid.AddForce(direct * force);
@@ -41,5 +72,11 @@ public class VirusController : MonoBehaviour
     void ResetBall() {
         transform.localPosition = new Vector2(0, 0);
         rigid.velocity = new Vector2(0, 0);
+    }
+
+     void TampilkanScore () {
+        Debug.Log ("Score White Hat: " + scoreP1 + " Score Blackhat: " + scoreP2);
+        scoreUIP1.text = scoreP1 + "";
+        scoreUIP2.text = scoreP2 + "";
     }
 }
